d1c7328d93279c35fee1fe2835220d4d01a5560f
maiaposternack22 <maia.posternack22@trinityschoolnyc.org>
2021-11-18 09:19:15 -0500

    dynamically make pages

--- ejs_demo.js
+++ ejs_demo.js
@@ -5,14 +5,20 @@ const ejs = require('ejs');
 
 let data_info = fs.readFileSync('compiled.json', 'utf8');
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
+/*for(let a of JSON.parse(data_info)){
+  console.log(a)
+}*/
+//console.log(JSON.parse(data_info)[1])
+for(let a of JSON.parse(data_info)){
 
 // put in for loop that goes 10 times and makes 10 identical pages for now
 let index_html = ejs.render(index_template, {
   filename: __dirname + '/views/index.ejs',
-  data: JSON.parse(data_info)
+  data: a
 });
 
 //only thing in here that would have to change is like build/[new name].html for each round of loop
-fs.writeFileSync('build/index.html', index_html, 'utf8');
+fs.writeFileSync('build/'+a.state+'.html', index_html, 'utf8');
+}
 
 //end of for loop
