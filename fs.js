f0147973b6ebc6518a7861a7db47608389702371
maiaposternack22 <maia.posternack22@trinityschoolnyc.org>
2021-11-17 21:35:13 -0500

    dynamically making websites

---
+++ fs.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+let state_data = [];
+
+let states_csv = fs.readFileSync('data.csv', 'utf8');
+
+let states = states_csv.split("\n");
+
+states.forEach(function(state) {
+  let state_info = state.split(',');
+  let data = {};
+  data['state'] = state_info[0];
+  data['total'] = state_info[1];
+  data['population'] = state_info[2];
+  data['ratio'] = state_info[3];
+
+  state_data.push(data);
+});
+
+fs.writeFileSync('compiled.json', JSON.stringify(state_data), 'utf8');
