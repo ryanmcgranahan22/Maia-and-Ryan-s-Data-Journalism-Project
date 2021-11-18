5b1a9e2720d278ec8efdc87f4cd57fcc87a09e03
ryanmcgranahan22 <56641574+ryanmcgranahan22@users.noreply.github.com>
2021-11-18 09:11:02 -0500

    update

--- fs.js
+++ fs.js
@@ -13,6 +13,17 @@ states.forEach(function(state) {
   data['total'] = state_info[1];
   data['population'] = state_info[2];
   data['ratio'] = state_info[3];
+  data['firearm'] = state_info[4];
+  data['cutPierce'] = state_info[5];
+  data['struckByAgainst'] = state_info[6];
+  data['poisoning'] = state_info[7];
+  data['hangingSuffocation'] = state_info[8];
+  data['fall'] = state_info[9];
+  data['drowning'] = state_info[10];
+  data['fireBurn'] = state_info[11];
+  data['motorVehicle'] = state_info[12];
+  data['other'] = state_info[13];
+  data['unknown'] = state_info[14];
 
   // continue making like data['gun']= state_info[4] etc for all columns of data
 
