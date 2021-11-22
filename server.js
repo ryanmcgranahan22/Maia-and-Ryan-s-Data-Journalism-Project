f1d8ed49238f254c3afcbc29d06bf9dd3d5fb167
maiaposternack22 <maia.posternack22@trinityschoolnyc.org>
2021-11-22 14:42:04 -0500

    pushing :)

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('static')); //greenlights all requests for files in the static folder
+
+app.listen(port, () => {
+  console.log(`Data server listening at http://localhost:${port}`)
+});
