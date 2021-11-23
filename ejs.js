aa542ef0ad220120578047547880c4a420cf5854
maiaposternack22 <maia.posternack22@trinityschoolnyc.org>
2021-11-23 09:30:08 -0500

    bootstrapped navbar

--- ejs.js
+++ ejs.js
@@ -4,8 +4,10 @@ const fs = require('fs');
 const ejs = require('ejs');
 
 let data_info = fs.readFileSync('compiled.json', 'utf8');
-let index_template = fs.readFileSync('views/index.ejs', 'utf8');
-let macro_template = fs.readFileSync('views/main_index.ejs', 'utf8');
+let index_template = fs.readFileSync('views/micro.ejs', 'utf8');
+let macro_template = fs.readFileSync('views/index.ejs', 'utf8');
+let about_template = fs.readFileSync('views/about.ejs', 'utf8');
+
 /*for(let a of JSON.parse(data_info)){
   console.log(a)
 }*/
@@ -14,7 +16,7 @@ for(let a = 1; a < JSON.parse(data_info).length; a++){
 
 // put in for loop that goes 10 times and makes 10 identical pages for now
 let index_html = ejs.render(index_template, {
-  filename: __dirname + '/views/index.ejs',
+  filename: __dirname + '/views/micro.ejs',
   data: JSON.parse(data_info)[a]
 });
 
@@ -24,10 +26,17 @@ fs.writeFileSync('build/micro'+a+'.html', index_html, 'utf8');
 }
 
 let macro_html = ejs.render(macro_template, {
-  filename: __dirname + '/views/main_index.ejs',
+  filename: __dirname + '/views/index.ejs',
   data: JSON.parse(data_info)
 });
 
 fs.writeFileSync('build/index.html', macro_html, 'utf8');
+
+let about_html = ejs.render(about_template, {
+  filename: __dirname + '/views/about.ejs',
+  data: JSON.parse(data_info)
+});
+
+fs.writeFileSync('build/about.html', about_html, 'utf8');
 
 //end of for loop
