0749e492970d887891aa6e12565b7d9a17118789
maiaposternack22 <maia.posternack22@trinityschoolnyc.org>
2021-11-27 12:36:06 -0500

    fixed header to incude states when in micropages

--- ejs.js
+++ ejs.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const ejs = require('ejs');
 
 let data_info = fs.readFileSync('compiled.json', 'utf8');
-let index_template = fs.readFileSync('views/micro.ejs', 'utf8');
+let micro_template = fs.readFileSync('views/micro.ejs', 'utf8');
 let macro_template = fs.readFileSync('views/index.ejs', 'utf8');
 let about_template = fs.readFileSync('views/about.ejs', 'utf8');
 
@@ -12,16 +12,22 @@ let about_template = fs.readFileSync('views/about.ejs', 'utf8');
   console.log(a)
 }*/
 //console.log(JSON.parse(data_info)[1])
+
+
+
 for(let a = 1; a < JSON.parse(data_info).length; a++){
 
+  let data1 = JSON.parse(data_info)
+  data1["page"] = a
 // put in for loop that goes 10 times and makes 10 identical pages for now
-let index_html = ejs.render(index_template, {
+let micro_html = ejs.render(micro_template, {
   filename: __dirname + '/views/micro.ejs',
-  data: JSON.parse(data_info)[a]
+  //data: JSON.parse(data_info)[a]
+  data: data1
 });
 
 //only thing in here that would have to change is like build/[new name].html for each round of loop
-fs.writeFileSync('build/micro'+a+'.html', index_html, 'utf8');
+fs.writeFileSync('build/micro'+a+'.html', micro_html, 'utf8');
 
 }
 
